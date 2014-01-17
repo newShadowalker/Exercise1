baa623d394b31cf1edd4dc7b641091207353296b
Roman Gorlach <roman.gorlach@gmail.com>
2014-01-17 18:33:09 -0500

    lines with protected and private were put in the comments; line with public was added

--- OOP344-Exercise1/Manager.h
+++ OOP344-Exercise1/Manager.h
@@ -4,8 +4,9 @@ class Manager
    {
    private:
       int totalHours;
-   protected:
-   private:
+   //protected: should be as public
+   //private: could not be two times private and the following should be as public
+   public: //added line 
       Manager();
 
       void getWorkerHours(Employee* emp);
